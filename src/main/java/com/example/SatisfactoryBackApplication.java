49bd21ee6c7dc16dc8751c9ba454716545c409f5
Omar <omar.12200336@gmail.com>
2022-05-11 11:53:04 +0600

    make it better

--- src/main/java/com/example/SatisfactoryBackApplication.java
+++ src/main/java/com/example/SatisfactoryBackApplication.java
@@ -1,12 +1,16 @@
 package com.example;
 
+import com.example.task.TaskController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
 
 @SpringBootApplication
 public class SatisfactoryBackApplication {
 
     public static void main(String[] args) {
+        new File(TaskController.uploadDirectory).mkdir();
         SpringApplication.run(SatisfactoryBackApplication.class, args);
     }
 
--- src/main/java/com/example/task/TaskController.java
+++ src/main/java/com/example/task/TaskController.java
@@ -8,14 +8,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
 public class TaskController {
     @Autowired private TaskService service;
+
+    public static String uploadDirectory = System.getProperty("user.dir")+"/uploads";
 
     @GetMapping("/tasks")
     public String showTasksList(Model model) {
@@ -41,7 +49,13 @@ public class TaskController {
     }
 
     @PostMapping("/tasks/save")
-    public String saveTask(Task task, RedirectAttributes ra) {
+    public String saveTask(Task task, RedirectAttributes ra, Model model, @RequestParam("files") MultipartFile[] files) throws IOException {
+        StringBuilder fileNames = new StringBuilder();
+        for (MultipartFile file: files){
+            Path fileNameAndPath = Paths.get(uploadDirectory, file.getOriginalFilename());
+            fileNames.append(file.getOriginalFilename());
+            Files.write(fileNameAndPath, file.getBytes());
+        }
         service.save(task);
         ra.addFlashAttribute("message", "The task has been successfully created");
         return "redirect:/tasks";
